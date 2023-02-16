123f486aa08c41992946ff88d68572927bfb2348
THEKINGSTAR <THEKINGSTAR@GMAIL.COM>
2023-02-17 00:24:40 +0200

    5. Hello, printf 5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
