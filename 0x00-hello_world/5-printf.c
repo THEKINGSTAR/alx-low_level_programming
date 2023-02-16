e48dae97d68f0bcc134396634d86f2c817afd2b7
THEKINGSTAR <THEKINGSTAR@GMAIL.COM>
2023-02-17 00:25:52 +0200

    Fix #1 IN 5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{printf("with proper grammar, but the outcome is a piece of art,");
+{printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
