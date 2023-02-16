2084eedace3e4232e75b28fa380b2dc20afc9c4b
THEKINGSTAR <THEKINGSTAR@GMAIL.COM>
2023-02-16 23:48:27 +0200

    fix # 1 IN 4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-int main (void)
-{	
-  char str1[90] ;
-  strcpy(str1, "\"Programming is like building a multilingual puzzle\n");
-  puts(str1);	
- 
- return 0;
+int main(void)
+{char str1[90];
+strcpy(str1, "\"Programming is like building a multilingual puzzle\n");
+puts(str1);
+return (0);
 }
