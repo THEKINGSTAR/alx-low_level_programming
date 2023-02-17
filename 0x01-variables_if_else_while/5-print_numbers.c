43a2f3efe3fea3b7f235543aa367b1e256370d37
THEKINGSTAR <THEKINGSTAR@GMAIL.COM>
2023-02-17 23:40:12 +0200

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i = 0;
+for (i; i < 10; i++)
+{
+printf("%d", i);
+}
+return (0);
+}
