98b56ee38008ed9eb81db756ec72c98a224da7d3
THEKINGSTAR <THEKINGSTAR@GMAIL.COM>
2023-02-17 23:58:42 +0200

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* more headers goes there */
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ch;
+for (ch = 0 ; ch <= 9 ; ch++)
+{
+ch = ch % 100;
+ch = ch % 10;
+putchar(ch + '0');
+}
+ch = '\n';
+putchar (ch);
+return (0);
+}
