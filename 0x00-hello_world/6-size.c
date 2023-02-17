45dd119ccb8bb9ac247a6e4bffd67a45941af4e2
THEKINGSTAR <THEKINGSTAR@GMAIL.COM>
2023-02-17 02:49:10 +0200

    FIX # 5 in : 6. Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <float.h>
+#include <string.h>
 /**
 * main - Entry point
 *
@@ -15,10 +16,10 @@ float floatType;
 double doubleType;
 char charType;
 /*Sizeof operator is used to evaluate the size of a variable*/
-printf("Size of char: %ld byte(s)\n", sizeof(charType));
-printf("Size of int: %ld bytes(s)\n", sizeof(integerType));
-printf("Size of long int: %ld bytes(s)\n", sizeof(longType));
-printf("Size of long long: %lld bytes(s)\n", sizeof(longType)*2);
-printf("Size of float: %ld bytes(s)\n", sizeof(floatType));
+printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+printf("Size of an int: %ld bytes(s)\n", sizeof(integerType));
+printf("Size of a long int: %ld bytes(s)\n", sizeof(long));
+printf("Size of a long long int: %lld bytes(s)\n", sizeof(long));
+printf("Size of a float: %ld bytes(s)\n", sizeof(floatType));
 return (0);
 }
