abc9a43927bef49486c1ac161ec959ec00d7c818
THEKINGSTAR <THEKINGSTAR@GMAIL.COM>
2023-02-17 03:30:35 +0200

    Fix #4 : 8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
 int main(void)
 {
 fprintf(stderr,
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19", 60);
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 return (1);
 }
