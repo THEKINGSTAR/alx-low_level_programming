91ae2c02d525b76d0fd2eedc423a358c97cfa528
THEKINGSTAR <THEKINGSTAR@GMAIL.COM>
2023-02-18 01:47:16 +0200

    FIX #1 BETTY STYLE : 11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,39 +11,33 @@
 int main(void)
 {
 int ch1;
-int ch2 ;
-int jump =1;
+int ch2;
+int jump = 1;
 for (ch1 = 0 ; ch1 <= 9 ; ch1++)
 {
-for ( ch2=jump ; ch2 <= 9 ;ch2++ )
+for (ch2 = jump ; ch2 <= 9 ; ch2++)
 {
 	if (ch1 != ch2)
 	{
-	ch1 = ch1 % 100;
-	ch1 = ch1 % 10;
-	ch2 = ch2 % 100;
-	ch2 = ch2 % 10;
-	putchar(ch1 + '0');
-	putchar(ch2 + '0');
- 
-	
-	if ( (ch1 == 8)  && (ch2 == 9 ))
+		ch1 = ch1 % 100;
+		ch1 = ch1 % 10;
+		ch2 = ch2 % 100;
+		ch2 = ch2 % 10;
+		putchar(ch1 + '0');
+		putchar(ch2 + '0');
+	if ((ch1 == 8)  && (ch2 == 9))
 	{
-	  break;
+	break;
 	}
 	else
 	{
 	putchar(44);
 	putchar(32);
 	}
-	
 	}
-	
-	
- }
-	jump+=1;
 }
-
+jump += 1;
+}
 ch1 = '\n';
 putchar (ch1);
 return (0);
