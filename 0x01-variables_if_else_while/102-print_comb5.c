1886dfdc650c2975d6f9ed5bd6fde5dcc95f46e6
THEKINGSTAR <THEKINGSTAR@GMAIL.COM>
2023-02-19 00:34:22 +0200

    FIX #8 IN : 12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -51,3 +51,62 @@ int main(void)
         }    
     return (0);
 }
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int ch1 , ch2, ch3 ,ch4;
+
+    for (ch1=0 ; ch1 < 10; ch1++)
+    {
+        for ( ch2 =0; ch2 < 10; ch2++)
+        {
+            for (ch3=0 ; ch3 < 10; ch3++)
+            {
+                for (ch4=1; ch4 < 10; ch4++)
+                {
+                /*
+                    00 01 
+                            and 
+                    01 00 are considered as the same combination of the numbers 0 and 1
+                */
+                    ch1 = ch1 % 100;
+                    ch1 = ch1 % 10;
+
+                    ch2 = ch2 % 100;
+                    ch2 = ch2 % 10;
+
+                    ch3 = ch3 % 100;
+                    ch3 = ch3 % 10;
+
+                    ch4 = ch4 % 100;
+                    ch4 = ch4 % 10;
+
+
+                        putchar(ch1 + '0');
+                        putchar(ch2 + '0');
+
+                        putchar(32);
+
+                        putchar(ch3 + '0');
+                        putchar(ch4 + '0');
+
+                        putchar(44);
+                        putchar(32);
+
+
+                        
+                    }
+                }
+           }
+           
+        }    
+    return (0);
+}
