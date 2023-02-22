41c40011fb6100baa46dba72b20517f1f49340f4
THEKINGSTAR <thekingstar5002@gmail.com>
2023-02-22 02:20:53 +0200

    FIX #2 -> STYLE : 1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
-* print alphabet
+* print_alphabet - output dunction
 *
-* Description: 'functcion return the alphaet in asseending order'
+* Description:: 'functcion return the alphaet in
+* *asseending order and print new line '
 *
 * Return: function has no return values
 */
 void print_alphabet(void)
 {
 	char ch;
-	for(ch = 'a'; ch <= 'z'; ch++)
+
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
 		_putchar(ch);
 	}
