4da5f07e4a3b0d0634ab0d6c384fe2fa95433493
THEKINGSTAR <thekingstar5002@gmail.com>
2023-02-22 05:47:25 +0200

    final fix IN : 9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,22 +2,32 @@
 /**
 *  times_table- output function
 *
-* Description :'Write a function that prints the 9 times table, starting with 0.'
+* Description :'Write a function that prints the 9 times table,
+* starting with 0.'
 *
 * Return: function has no return values
 */
 void times_table(void)
 {
-	int ones ;
+	int ones, multi;
 
-	for (ones = 0; ones < 10 ; ones++)
+	for (ones = 0; ones < 10; ones++)
 	{
 		int tens;
 
-		for (tens = 0 ; tens < 10 ; tens ++)
-		{		
-			_putchar(ones + '0');
-			_putchar(tens + '0');
+		for (tens = 0 ; tens < 10 ; tens++)
+		{
+			multi = ones * tens;
+			if (multi <= 9)
+			{
+			_putchar(' ');
+			_putchar(multi + '0');
+			}
+			else if (multi > 9)
+			{
+				_putchar(multi / 10 + '0');
+				_putchar(multi % 10 + '0');
+			}
 			_putchar(',');
 			_putchar(' ');
 		}
