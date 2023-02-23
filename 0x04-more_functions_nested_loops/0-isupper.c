35fe8d349657567b66a59c3bd0b65d1b89f202cf
THEKINGSTAR <thekingstar5002@gmail.com>
2023-02-24 01:58:17 +0200

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*_isalpha - output function
+*@c: An input character
+* Description:'function to check if the curters is upercase or not'
+*
+* Return: function return 1 if the character is upeercase
+* * & return 0 if not
+*/
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+	return (1);
+else
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,19 @@
+#ifndef HEADER_putchar
+#define HEADER_putchar
+
+#include <stdio.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+void jack_bauer(void);
+int print_last_digit(int);
+void times_table(void);
+int add(int, int);
+int _isupper(int c);
+
+#endif
