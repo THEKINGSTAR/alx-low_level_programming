766da4e3f761f96532ca65a2ba306ca70141abaf
THEKINGSTAR <thekingstar5002@gmail.com>
2023-02-24 04:01:11 +0200

    finla fix

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,31 +3,32 @@
 * print_diagonal - output dunction
 *
 * @n : is the number of times will print \ in diagonla
-* Description:: 'that draws a diagonal line in the terminal'
+* Description:'that draws a diagonal line in the terminal'
 *
 * Return: function has no return values
 */
 void print_diagonal(int n)
 {
-	int l = n;
-
-	if (l <= 0)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
-	else if (l > 0)
+	else if (n > 0)
 	{
-		for (l = 1; l <= n; l++)
+		int l;
+
+		for (l = 0; l < n; l++)
 		{
 			int i;
 
-			for (i = 1; i < l; i++)
+			for (i = 0; i < l; i++)
 			{
 				_putchar(' ');
 			}
+
 			_putchar('\\');
 			_putchar('\n');
+
 		}
-		_putchar('\n');
 	}
 }
