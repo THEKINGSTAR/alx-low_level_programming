9488dd8d55214c1d8d66af540b73776d68c8caf7
THEKINGSTAR <thekingstar5002@gmail.com>
2023-02-24 06:40:09 +0200

    fix 4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* print_number - output dunction
+* print_most_numbers - output dunction
 *
 * Description:: 'function to print the number
-* *asseending order for 0 to 9 exept 2 and 4 
+* *asseending order for 0 to 9 exept 2 and 4
 * and print new line '
 *
 * Return: function has no return values
@@ -14,7 +14,7 @@ void print_most_numbers(void)
 
 	for (n = 0; n <= 9; n++)
 	{
-		if (n == 2 || n==4)
+		if (n == 2 || n == 4)
 		{
 			continue;
 		}
