2f0ea9b6d76e7cc3496449507f1b7870d9abee1c
THEKINGSTAR <thekingstar5002@gmail.com>
2023-02-24 02:28:20 +0200

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_number - output dunction
+*
+* Description:: 'function to print the number
+* *asseending order for 0 to 9 and print new line '
+*
+* Return: function has no return values
+*/
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -17,5 +17,6 @@ int add(int, int);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
+void print_numbers(void);
 
 #endif
