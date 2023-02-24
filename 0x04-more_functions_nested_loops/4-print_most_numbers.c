6f7bc48ad6cddcaa4d9a9853c307082e38360174
THEKINGSTAR <thekingstar5002@gmail.com>
2023-02-24 02:32:02 +0200

    4. I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_number - output dunction
+*
+* Description:: 'function to print the number
+* *asseending order for 0 to 9 exept 2 and 4 
+* and print new line '
+*
+* Return: function has no return values
+*/
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n == 2 || n==4)
+		{
+			continue;
+		}
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
