e31003cd415d173d60a844c83bedd4fc784c3dbb
THEKINGSTAR <thekingstar5002@gmail.com>
2023-02-24 02:18:32 +0200

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Check Holberton
+ * @a: An integer a
+ * @b: An integer b
+ * Description: Function that multiply two integers 
+ * and returns the result
+ * Return: Result to multiply a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,5 +16,6 @@ void times_table(void);
 int add(int, int);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
 
 #endif
