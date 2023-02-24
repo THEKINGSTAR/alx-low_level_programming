903d8892721d55c2b6ceb83545bf0de32c5ed41b
THEKINGSTAR <thekingstar5002@gmail.com>
2023-02-24 04:53:28 +0200

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+* print_diagonal - output dunction
+*
+* @size : the size of the triangle
+* Description:'that prints a triangle, followed by a new line.'
+*
+* Return: function has no return values
+*/
+void print_triangle(int size)
+{
+	int n = size;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else if (n > 0)
+	{
+		int l;
+
+		for (l = 1; l <= n; l++)
+		{
+			int i,s,p = n;
+
+			for (i = 0; i <= p; i++)
+			{
+				_putchar(' ');
+			}
+			p--;
+			for (s = 0; s <= l; s++)
+                        {
+                                _putchar('#');
+                        }
+
+			_putchar('\n');
+
+		}
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -24,4 +24,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 
+void print_triangle(int size);
+
 #endif
