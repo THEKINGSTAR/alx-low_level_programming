65b49815230375ea39da98d670d3cd1ebbb8905c
THEKINGSTAR <thekingstar5002@gmail.com>
2023-02-28 00:47:12 +0200

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,13 @@
 #ifndef HEADER_FILE
 #define HEADER_FILE
 
-
+#include <unistd.h>
 #include <stdio.h>
 
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-
+void _puts(char *str);
 
 #endif
