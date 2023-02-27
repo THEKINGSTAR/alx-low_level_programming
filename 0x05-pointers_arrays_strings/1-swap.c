4c41559f1f383b0676881be0703163eab4d05986
THEKINGSTAR <thekingstar5002@gmail.com>
2023-02-27 22:51:50 +0200

    1. Don't swap horses in crossing a stream

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  swap_int - wsap function
+ *
+ * @a: the input parameter we want to modifie
+ * @b: the input tath will swap with
+ * Description:'Write a function that swaps the values of two integers.'
+ *
+ * Return: function has no return values
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *b - *a;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
 
 
 void reset_to_98(int *n);
-
+void swap_int(int *a, int *b);
 
 
 #endif
