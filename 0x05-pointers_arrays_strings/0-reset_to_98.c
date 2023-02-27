ae8fc30e5c928988fb7dd93e47920e077f7e4491
THEKINGSTAR <thekingstar5002@gmail.com>
2023-02-27 22:42:40 +0200

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98- input function
+ * @n: the input parameter we want to modifie
+ * Description:'Write a function that takes a pointer to an int as parameter
+ * and updates the value it points to to 98.'
+ *
+ * Return: function has no return values
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+
+#include <stdio.h>
+
+
+void reset_to_98(int *n);
+
+
+
+#endif
