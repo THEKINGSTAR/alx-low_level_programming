6aa972b61bb0338340155c320821cd92cf81a77b
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-06 23:30:34 +0200

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,34 @@
+#include  "main.h"
+
+
+/**
+ * _strchr-memory locating function
+ *
+ * @s:string to be serch in
+ * @c:character to be found
+ *
+ * Description:'fuction to search string with charcter
+ * and return poihter to found location'
+ *
+ * Return:return a pointer to the memory of found location
+ *
+ */
+
+char *_strchr(char *s, char c)
+{
+	char *ptr;
+
+	ptr = NULL;
+	while (*s)
+	{
+		if (*s == c)
+		{
+			ptr = s;
+			return (ptr);
+		}
+		s++;
+	}
+
+	return (ptr);
+
+}
