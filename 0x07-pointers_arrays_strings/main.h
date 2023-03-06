37e9a433d2294d0449897a977c8c20ef5144658c
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-06 23:08:35 +0200

    0. memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,25 @@
+#ifndef HEADER_FILE
+#define HEADER_FILe
+
+#include <unistd.h>
+#include <stdio.h>
+
+
+int _putchar(char c);
+
+char *_memset(char *s, char b, unsigned int n);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
