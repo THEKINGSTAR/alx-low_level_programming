c05ed76b53bfd17031bba51ee22aa719124b949b
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-09 05:06:09 +0200

    fix style

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,15 @@
 #include "main.h"
-
-
+/**
+ * root_calculate - return function
+ *
+ * @n:the input number
+ * @root: the start calcutae from
+ * Description:'Write a function that returns the
+ * natural square root of a number.'
+ *
+ * Return:f n does not have a natural square root,
+ * the function should return -1.
+ */
 int root_calculate(int root, int n)
 {
 	if (root * root > n)
@@ -19,7 +28,7 @@ int root_calculate(int root, int n)
  * _sqrt_recursion - return function
  *
  * @n:the input number
- * 
+ *
  * Description:'Write a function that returns the
  * natural square root of a number.'
  *
