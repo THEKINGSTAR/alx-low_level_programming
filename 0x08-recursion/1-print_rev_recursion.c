457d36337a804795e3a71814f116ba2572f53560
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-09 02:13:18 +0200

    fix the new line

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,6 +1,29 @@
 #include "main.h"
 
 /**
+ * revers - output function
+ *
+ * @s:the input string
+ *
+ * Description:'fucnction that prints a string in reverse.'
+ *
+ * Return: function has no return values
+ */
+void revers(char *s)
+{
+	char *ptr;
+
+	ptr = s;
+	if (*ptr != '\0')
+	{
+		ptr++;
+		revers(ptr);
+		putchar(*ptr);
+		return;
+	}
+}
+
+/**
  *  _print_rev_recursion- output function
  *
  * @s:the input string
@@ -11,10 +34,22 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s != '\0')
-	{
-		s++;
-		_print_rev_recursion(s);
-		putchar(*s);
-	}
+	/*
+	 * char *ptr;
+	 *
+	 * ptr = s;
+	 *
+	 * if (*ptr != '\0')
+	 * {
+	 *	ptr++;
+	 *	_print_rev_recursion(ptr);
+	 *	putchar(*ptr);
+	 *	return ;
+	 *	}
+	 */
+	revers(s);
+	putchar('\n');
 }
+
+
+
