8862cd2a12907b0ce91f786ab009fa9407834fc4
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-11 04:24:08 +0200

    4. To infinity and beyond

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * @argc:argc will be the number of strings
+ * @argv:strings pointed to by argv
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+	int i, sum, num;
+
+	if (argc > 1)
+	{
+		sum = 0;
+		for (i = 0; i < argc; i++)
+		{
+			num = atoi(argv[i]);
+			sum += num;
+		}
+		printf("%d\n", sum);
+	}
+	else
+	{
+		printf("0\n");
+	}
+
+	return (0);
+}
