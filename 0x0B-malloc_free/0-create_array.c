9d13c733b5db964c31ece0da70b58c0663875eaf
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-14 16:47:42 +0200

    0. Float like a butterfly, sting like a bee

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * create_array - prints buffer in hexa
+ *
+ * @c: the the char to fil the array with
+ * @size: the size of the memory to creat the array with
+ *
+ * Description:'Write a function that creates an array of chars,
+ * and initializes it with a specific char.
+ * Returns NULL if size = 0'
+ *
+ * Return: Returns a pointer to the array, or NULL if it fails.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	int i, arsiz;
+
+	if (size <= 0)
+	{
+		ptr = NULL;
+	}
+	else if (size > 0)
+	{
+		char *arr;
+
+		arsiz = size;
+		arr = malloc(sizeof(*arr) * arsiz);
+		ptr = arr;
+		for (i = 0; i < arsiz ; i++)
+		{
+			arr[i] = c;
+		}
+	}
+	return (ptr);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,9 +2,9 @@
 #define HEADER_FILE
 
 #include <stdio.h>
-
+#include <stdlib.h>
 
 char *_strdup(char *str);
-
+char *create_array(unsigned int size, char c);
 
 #endif
