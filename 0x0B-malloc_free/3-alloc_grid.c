e7ed83bc9f8fd4a80ceee9efac81a3d4a1fe6c37
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-15 05:00:45 +0200

    fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,7 +20,7 @@ int **alloc_grid(int width, int height)
 
 	if (!(width <= 0) || !(height <= 0))
 	{
-		ptr = (int **)malloc(sizeof(int) * width);
+		ptr = (int **)malloc(sizeof(int *) * width);
 		for (w = 0; w < width; w++)
 		{
 			ptr[w] = (int *)malloc(sizeof(int) * height);
