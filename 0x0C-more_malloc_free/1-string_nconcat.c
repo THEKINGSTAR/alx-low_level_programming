b61c86ba8cbf040577a481f6d5de4bc3c1174795
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-17 01:54:07 +0200

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,19 +1,86 @@
 #include "main.h"
+/**
+ *  _strlen - output function
+ *
+ * @crs: the input string
+ *
+ * Description:'Write a function that returns the length of a string.'
+ *
+ * Return: function has no return values
+ */
+int _strlen(char *crs)
+{
+	int len = 0;
+	char *cs;
 
+
+	cs = crs;
+	while (cs[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
 
 /**
- * string_nconcat -
- * Prototype: char *string_nconcat(char *s1, char *s2, unsigned int n);
- * The returned pointer shall point to a newly allocated space in memory,
- * which contains s1, followed by the first n bytes of s2, and null terminated
- * Return:  If the function fails, it should return NULL
- * If n is greater or equal to the length of s2 then use the entire string s2
+ * string_nconcat - copy string
+ *
+ * @s1: the first input string
+ * @s2: the second input string
+ * @n: followed by the first n bytes of s2
+ *
+ * Description:'Write a function that concatenates two strings.
+ * The returned pointer should point to a newly allocated space in memory
+ * which contains the contents of s1,
+ * followed by the contents of s2, and null terminated
  * if NULL is passed, treat it as an empty string
+ * If n is greater or equal to the length of s2 then use the entire string s2
+ * The function should return NULL on failure
+ *
+ * Return:newly allocated space in memory which contains the contents of s1,
+ * followed by the contents of s2, and null terminated.
+ *
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	int len1, len2, i, i2, n2 = n;
+	char *ptr, *newstr;
 
+	if (s1 == NULL)
+	len1 = 0;
+	else
+	{
+		len1 = _strlen(s1);
+	}
+	if (s2 == NULL)
+	len2 = 0;
+	else
+	{
+		len2 = _strlen(s2);
+	}
 
-
+	newstr = malloc((sizeof(char) * (len1)) + (sizeof(char) * (len2 + 1)));
+	if (newstr == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		ptr = newstr;
+		for (i = 0; i < len1 ; i++)
+		{
+			newstr[i] = s1[i];
+		}
+		if (n2 >= len2)
+		n2 = len2;
+		for (i2 = 0; i2 < n2; i2++)
+		{
+			newstr[i] = s2[i2];
+			i++;
+		}
+		newstr[i] = '\0';
+		return (ptr);
+		free(ptr);
+	}
+	return (NULL);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,6 +6,8 @@
 
 
 void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
 
 
 
