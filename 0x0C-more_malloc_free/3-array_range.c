2e5fdb2d973855d504537fbd864471610a946007
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-17 03:13:18 +0200

    fix memory size allocated

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -24,7 +24,7 @@ int *array_range(int min, int max)
 	{
 		return (NULL);
 	}
-	arr = malloc(sizeof(int) * max + 1);
+	arr = malloc(sizeof(int) * (max + 1));
 	if (arr == NULL)
 	{
 		return (NULL);
