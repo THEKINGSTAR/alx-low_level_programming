2fe98c603b4f34cb2b56443848977e8ae1dbd5d8
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-17 03:22:07 +0200

    arr_len = max - min ;

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -18,13 +18,13 @@
 int *array_range(int min, int max)
 {
 	int *arr;
-	int i;
+	int i, len = max - min;
 
 	if (min > max)
 	{
 		return (NULL);
 	}
-	arr = malloc(sizeof(int) * (max + 1));
+	arr = malloc(sizeof(arr) * len);
 	if (arr == NULL)
 	{
 		return (NULL);
