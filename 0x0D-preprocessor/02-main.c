07824292148a55f01b3a26fefe2ab58c8f602264
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-20 23:15:37 +0200

    2. File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * C program to print the name of
+ * source code file
+ */
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return 0;
+}
