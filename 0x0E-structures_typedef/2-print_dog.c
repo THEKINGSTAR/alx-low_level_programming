9665e37453a6624a7131a6c0620921f9e68d5f4e
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-21 02:02:11 +0200

    2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog - struct initializing function
+ * Description:'Write a function that prints a struct dog
+ * If an element of d is NULL,
+ * print (nil) instead of this element. (if name is NULL, print Name: (nil))
+ * If d is NULL print nothing.
+ * @d: struct of type dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: nill\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+		if (d->age == '\0')
+		{
+			printf("Age: nill");
+		}
+		else
+		{
+			printf("Age: %.1f\n", d->age);
+		}
+		if (d->owner == NULL)
+		{
+			printf("Owner: nill");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -19,6 +19,10 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+
+
 
 
 #endif
