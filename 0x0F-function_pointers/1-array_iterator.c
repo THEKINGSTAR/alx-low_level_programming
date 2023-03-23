7d331e54bac77548b52725360fb145e8e2850f8f
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-23 16:25:21 +0200

    fix 1-array_iterator.c check for null pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,7 +15,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i, arsiz;
 
-	if (array == NULL || size <= 0)
+	if (array == NULL || size <= 0 || action == NULL)
 	{
 		return;
 	}
