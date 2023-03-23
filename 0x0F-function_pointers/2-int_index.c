3d8f88a95dfe63e27e88440d37f1a87e50ed0fc3
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-23 05:21:14 +0200

    2. To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,43 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - fucntion to search array
+ * @array: array of elements
+ * @size: is the size of the array
+ * @cmp: the numper to be searched for
+ *
+ * Description:'Write a function that searches for an integer.'
+ * where size is the number of elements in the array array
+ * cmp is a pointer to the function to be used to compare values
+ *
+ * Return: int_index returns the index of the first element
+ * for which the cmp function does not return 0
+ * If no element matches, return -1
+ * If size <= 0, return -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, arsiz, ret, cmpval;
+
+	ret = 0;
+	if (array == NULL || size <= 0)
+	{
+		ret = -1;
+	}
+	else
+	{
+		arsiz = size;
+		/*printf("size is : %d\n", arsiz);*/
+		for (i = 0; i < arsiz; i++)
+		{
+			cmpval = cmp(array[i]);
+			/* printf("%d , %d \n", array[i], cmpval); */
+			if (array[i] != 0 && cmpval != 0)
+			{
+				ret = i;
+				return (ret);
+			}
+		}
+	}
+	return (ret);
+}
