6f34382fedef85c80df86b761f6509cee858bf2c
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-31 00:51:39 +0200

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * print_list - function to print the list
+ * @head : passed list
+ * Description:Write a function that prints all the elements of a list_t list.
+ *
+ * Format: see example
+ * If str is NULL, print [0] (nil)
+ * You are allowed to use printf
+ *
+ * Return: the number of nodes
+ *
+ */
+
+size_t print_list(const list_t *head)
+{
+	int size = 0;
+
+	if (head->str == NULL)
+	{
+		printf("[0] (nil)\n");
+		size++;
+	}
+	else
+	{
+		while (head != NULL)
+		{
+			if (head->str == NULL)
+			{
+				printf("[0] (nil)\n");
+				return (size++);
+			}
+			else
+			{
+				printf("[%u] %s\n", head->len, head->str);
+				size++;
+			}
+			head = head->next;
+		}
+	}
+	return (size);
+}
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "struct_list.h"
+
+int _putchar(char c);
+size_t print_list(const list_t *h);
+
+
+
+#endif
