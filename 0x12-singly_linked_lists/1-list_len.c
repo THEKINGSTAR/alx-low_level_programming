e978d70c6fc4ab8bc9fcd7142b3bce4443895545
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-31 04:32:50 +0200

    1. List length

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * list_len - function to return the list lenght
+ * list_len - returns the number of elements
  * @h : passed list
  * Description:Write a function that returns
  * the number of elements in a linked list_t list.
@@ -14,25 +14,10 @@ size_t list_len(const list_t *h)
 {
 	int size = 0;
 
-	if (h->str == NULL)
+	while (h != NULL)
 	{
-		printf("[0] (nil)\n");
 		size++;
-	}
-	else
-	{
-		while (h != NULL)
-		{
-			if (h->str == NULL)
-			{
-				return (size++);
-			}
-			else
-			{
-				size++;
-			}
-			h = h->next;
-		}
+		h = h->next;
 	}
 	return (size);
 }
