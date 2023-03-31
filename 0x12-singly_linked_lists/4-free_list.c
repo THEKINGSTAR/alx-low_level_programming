6db42e0b3f3f9805639f047f028adfb781492166
THEKINGSTAR <thekingstar5002@gmail.com>
2023-03-31 04:23:43 +0200

    4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * free_list - function to free alocated memory
+ *
+ * @head : passed list head node
+ *
+ * Description:Write a function that frees a list_t list.
+ *
+ * Return: function has no return values
+ *
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node, *preview_noed;
+			current_node = head;
+			while (current_node->next != NULL)
+			{
+				preview_noed = current_node;
+
+				current_node = current_node->next;
+				head = preview_noed;
+				free(head->str);
+				free(head);
+			}
+			free(current_node->str);
+			free(current_node);
+}
+
+
