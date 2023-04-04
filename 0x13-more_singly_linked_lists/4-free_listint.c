89debed071e0634b2b576cb431cb15c041a2e352
THEKINGSTAR <thekingstar5002@gmail.com>
2023-04-04 06:45:12 +0200

    4. Free list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * free_listint - count function
+ *
+ * Description: Write a function that adds
+ * a new node at the end of a listint_t list.
+ *
+ * Prototype: listint_t *add_nodeint_end(listint_t **head, const int n);
+ *
+ * @head: input list structure
+ *
+ * Return: the function has no return value
+ *
+ *
+ */
+void free_listint(listint_t *head)
+{
+	 listint_t *end, *current;
+
+	if (head == NULL)
+	{
+		return;
+	}
+
+	end = head;
+	while (end->next != NULL)
+	{
+		current = end;
+		end = end->next;
+		head = current;
+		free(head);
+	}
+}
