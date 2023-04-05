94f631b10406427b6525e7e592d9089eb715cdbb
THEKINGSTAR <thekingstar5002@gmail.com>
2023-04-05 04:48:49 +0200

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -56,7 +56,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	else if (idx <= len)
+	else if (idx <= len + 1)
 	{
 		current = *head;
 		while (ind != idx - 1)
