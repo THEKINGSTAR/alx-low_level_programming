b79bacd2c2c7f5b090201a1284c87726d51ea523
THEKINGSTAR <thekingstar5002@gmail.com>
2023-04-13 04:31:13 +0200

    commit fix 0 charchter clear

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,7 +22,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *file;
 	char c;
-	unsigned long int count = 0;
+	ssize_t count = 0;
 
 	file = fopen(filename, "r");
 
@@ -43,7 +43,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		}
 		fclose(file);
 	}
-	if (count < letters)
-		return (0);
 	return (count);
 }
