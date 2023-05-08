c3b5b51c2aba0c1617a02f08734e3d7c35deb4e5
THEKINGSTAR <thekingstar5002@gmail.com>
2023-05-09 01:30:08 +0300

    Fix end of line IN 0. Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char c;
 	ssize_t count = 0;
 
-	file = fopen(filename, "r");
+	file = fopen(filename, "w");
 
 	if (!file || file == NULL)
 	{
