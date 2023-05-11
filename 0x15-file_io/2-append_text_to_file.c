c6bf625006fdbbe1d18e8608a90c923f9be88772
THEKINGSTAR <thekingstar5002@gmail.com>
2023-05-11 15:54:26 +0300

    commit add O_RDWR | O_APPEND and add check for return value

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -72,13 +72,22 @@ int _append_text_to_file(const char *filename, char *text_content)
  * @text_content:is the NULL terminated string to add at the end of the file
  * @filename: is the file to read from
  *
- * Return:1 on success and -1 on failure
+ * Return:1 on success
+ * and
+ * -1 on failure
+ *
  * Do not create the file if it does not exist
+ *
  * If filename is NULL return -1
+ *
  * If text_content is NULL, do not add anything to the file.
- * Return 1 if the file exists and -1
- * if the file does not exist or
- * if you do not have the required permissions to write the file
+ *
+ * Return
+ * 1 if the file exists
+ * and
+ * -1 if the file does not exist
+ * or
+ * -1 if you do not have the required permissions to write the file
  *
  *
  */
@@ -98,8 +107,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (1);
 	}
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+	fd = open(filename, O_RDWR | O_APPEND);
+	if (fd < 0)
 	{
 		return (-1);
 	}
---
+++ 0x15-file_io/main_3.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - check the code .
+ *
+ * @ac: Arguments counter
+ * @av: Arguments array
+ *
+ * Return: 0 on success, 1 on error
+ */
+int main(int ac, char **av)
+{
+	int res;
+
+	if (ac < 2)
+	{
+		printf("Usage: %s filename\n", av[0]);
+		return (1);
+	}
+	res = append_text_to_file(av[1], NULL);
+	printf("%i\n", res);
+	return (0);
+}
