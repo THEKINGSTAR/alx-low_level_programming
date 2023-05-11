e408dc14f816722f0c8a9ad3254e14fe00eb4deb
THEKINGSTAR <thekingstar5002@gmail.com>
2023-05-11 16:14:16 +0300

    commit add O_RDWR | O_APPEND and add check for return value

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -103,15 +103,16 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL)
-	{
-		return (1);
-	}
 	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd < 0)
 	{
 		return (-1);
 	}
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+
 	num_written = write(fd, text_content, len);
 	if (num_written != len)
 	{
