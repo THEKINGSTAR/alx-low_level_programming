2a685761854f6d851bdbf8b9f958e994499eb88d
THEKINGSTAR <thekingstar5002@gmail.com>
2023-05-11 14:28:47 +0300

    fix file exists

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -89,30 +89,25 @@ int _append_text_to_file(const char *filename, char *text_content)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd = open(filename, O_WRONLY | O_APPEND);
 	int len;
 	int num_written;
 
-	/*vIf filename is NULL return -1 */
+	/* If filename is NULL return -1 */
 	if (filename == NULL)
-	{
 		return (-1);
-	}
+
 	if (text_content == NULL)
-	{
 		return (1);
-	}
+
 	if (access(filename, F_OK) == 0)
-	{
 		return (1);
-	}
 	else
 		return (-1);
+
 	if (access(filename, W_OK) == -1)
-	{
 		return (-1);
-	}
-	fd = open(filename, O_WRONLY | O_APPEND);
+
 	if (fd == -1)
 	{
 		return (-1);
