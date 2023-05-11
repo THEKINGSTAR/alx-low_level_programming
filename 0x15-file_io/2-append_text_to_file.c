a4b5270e8f7f22ec7b1a1995343d605b8b42d273
THEKINGSTAR <thekingstar5002@gmail.com>
2023-05-11 13:55:16 +0300

    fix file exists

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -97,14 +97,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (access(filename, W_OK) == -1)
-	{
-		return (-1);
-	}
 	if (access(filename, F_OK) == 0)
 	{
 		return (1);
 	}
+	else if (access(filename, W_OK) == -1)
+        {
+                return (-1);
+        }
 	if (text_content == NULL)
 	{
 		return (1);
