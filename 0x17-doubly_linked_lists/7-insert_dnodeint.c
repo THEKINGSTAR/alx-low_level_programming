121cb5ab35905d71d6fdfd10a6029824db5b5cac
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-16 08:18:55 +0300

    7. Insert at index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - insert node at index function
+ *
+ * @h: head of the list
+ * @idx: index to isert the node at
+ * @n: value to assign node
+ *
+ * where idx is the index of the list where the new node should be added.
+ * Index starts at 0
+ * Return: the address of the new node,
+ * or
+ * NULL if it failed
+ *
+ * if it is not possible to add the new node at index idx,
+ * do not add the new node and return NULL
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *current, *new_node;
+	int lenght = 0, index = idx;
+
+	current = *h;
+	new_node = NULL;
+	while (current != NULL)
+	{
+		current = current->next;
+		lenght++;
+	}
+	if ((index > lenght) || (!index))
+		return (NULL);
+	current = *h;
+	while (index - 1)
+	{
+		current = current->next;
+		index--;
+	}
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->n = n;
+
+	new_node->next = current->next;
+	current->next->prev = new_node;
+	current->next = new_node;
+	new_node->prev = current;
+	return (new_node);
+}
---
+++ 0x17-doubly_linked_lists/7-main.c
@@ -0,0 +1,56 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    dlistint_t *head;
+
+    head = NULL;
+    add_dnodeint_end(&head, 0);
+    add_dnodeint_end(&head, 1);
+    add_dnodeint_end(&head, 2);
+    add_dnodeint_end(&head, 3);
+    add_dnodeint_end(&head, 4);
+    add_dnodeint_end(&head, 98);
+    add_dnodeint_end(&head, 402);
+    add_dnodeint_end(&head, 1024);
+    print_dlistint(head);
+    printf("-----------------\n");
+    insert_dnodeint_at_index(&head, 5, 4096);
+    print_dlistint(head);
+    free_dlistint(head);
+    head = NULL;
+    return (EXIT_SUCCESS);
+}
+/**
+ *
+ * gcc -Wall -pedantic -Werror -Wextra -std=gnu89 7-main.c 2-add_dnodeint.c 3-add_dnodeint_end.c 0-print_dlistint.c 4-free_dlistint.c 7-insert_dnodeint.c -o j
+ * 
+ *
+ * julien@ubuntu:~/0x17. Doubly linked lists$ ./j 
+0
+1
+2
+3
+4
+98
+402
+1024
+-----------------
+0
+1
+2
+3
+4
+4096
+98
+402
+1024
+*/
