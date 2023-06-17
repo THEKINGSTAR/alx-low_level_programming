b553e23bdb21a6f0cc07018d8f64cdb2453479bd
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-17 05:44:24 +0300

    7. Insert at index

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -44,7 +44,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	
+
 	new_node->next = current->next;
 	current->next->prev = new_node;
 	current->next = new_node;
