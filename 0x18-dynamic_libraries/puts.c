f793900c42dbad82d97bd8924f9c0d8117430f90
THEKINGSTAR <thekingstar5002@gmail.com>
2023-06-21 11:46:02 +0300

    FIX MISSING FUNCTIION IN TASK : 0. A library is not a luxury but one of the necessities of life

---
+++ 0x18-dynamic_libraries/puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *  _puts - output function
+ *
+ * @str: the input string
+ *
+ * Description:'Write a function that print string follow new line.'
+ *
+ * Return: function has no return values
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+		++str;
+	}
+	putchar('\n');
+}
