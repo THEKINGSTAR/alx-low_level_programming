6e97c0c2136dedf0f03e4c8cd8a877e832936e72
THEKINGSTAR <thekingstar5002@gmail.com>
2023-07-06 21:23:18 +0300

    0. >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+
+/**
+ * hash_table_create - Write a function that creates a hash table.
+ *
+ * Prototype: hash_table_t *hash_table_create(unsigned long int size);
+ *
+ * @size: is the size of the array
+ *
+ * Return: a pointer to the newly created hash table
+ * If something went wrong, your function should return NULL
+ */
+
+
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hs_tb;
+	unsigned long int i = 0;
+
+	hs_tb = malloc(sizeof(hash_table_t));
+	if (hs_tb == NULL)
+		return (NULL);
+
+	hs_tb->size = size;
+	hs_tb->array = malloc(sizeof(hash_node_t) * size);
+	if (hs_tb->array == NULL)
+		return (NULL);
+
+	while (size--)
+	{
+		hs_tb->array[i] = NULL;
+		i++;
+	}
+	return (hs_tb);
+}
---
+++ 0x1A-hash_tables/0-main.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code for
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    printf("%p\n", (void *)ht);
+    return (EXIT_SUCCESS);
+}
+
+
+/*
+julien@ubuntu:~/0x1A. Hash tables$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-main.c 0-hash_table_create.c -o a
+julien@ubuntu:~/0x1A. Hash tables$ ./a 
+0x238a010
+julien@ubuntu:~/0x1A. Hash tables$ valgrind ./a
+==7602== Memcheck, a memory error detector
+==7602== Copyright (C) 2002-2013, and GNU GPL'd, by Julian Seward et al.
+==7602== Using Valgrind-3.10.1 and LibVEX; rerun with -h for copyright info
+==7602== Command: ./a
+==7602== 
+0x51fc040
+==7602== 
+==7602== HEAP SUMMARY:
+==7602==     in use at exit: 8,208 bytes in 2 blocks
+==7602==   total heap usage: 2 allocs, 0 frees, 8,208 bytes allocated
+==7602== 
+==7602== LEAK SUMMARY:
+==7602==    definitely lost: 16 bytes in 1 blocks
+==7602==    indirectly lost: 8,192 bytes in 1 blocks
+==7602==      possibly lost: 0 bytes in 0 blocks
+==7602==    still reachable: 0 bytes in 0 blocks
+==7602==         suppressed: 0 bytes in 0 blocks
+==7602== Rerun with --leak-check=full to see details of leaked memory
+==7602== 
+==7602== For counts of detected and suppressed errors, rerun with: -v
+==7602== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
+julien@ubuntu:~/0x1A. Hash tables$
+*/
